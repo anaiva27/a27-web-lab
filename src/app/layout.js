23c9e6c5773fb3e3ad52a07d8c7e17a88c340e35
anaiva27 <nastia.iv.91@gmail.com>
2026-09-11 10:41:32 -0700

    update meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,10 @@ import Footer from "@/components/Footer/Footer";
 export const metadata = {
 	title: "A27 Web Lab | Tech Company",
 	description:
-		"@ A27 Web Lab we offer powerful digital solutions and web strategies",
+		"@ A27 Web Lab we offer powerful websites and web strategies that convert visitors to clients to bring you more sales, trust and prosperity.",
+	other: {
+		"trustpilot-one-time-domain-verification-id": "YOUR_VERIFICATION_ID",
+	},
 };
 
 export default function RootLayout({ children }) {
